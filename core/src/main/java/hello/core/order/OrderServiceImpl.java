f03bce0f4f29b54a63005bd09b2791b7176d92e8
yeseul106 <20191037@sungshin.ac.kr>
2023-04-15 02:22:26 +0900

    [Section2] 주문과 할인 도메인 개발

---
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,19 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService{
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice); //OCP 잘 지킴. 할인과 관련된 역할은 discountPolicy에게 다 맡김
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
