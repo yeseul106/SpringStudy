f04d37e33fb1630e4bac67bd61fadf9c67badd51
yeseul106 <20191037@sungshin.ac.kr>
2023-04-15 01:50:55 +0900

    [Section2] 회원 도메인 실행과 테스트

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,6 +2,8 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
+    //실제 할당하는 부분이 추상화에도 의존하고, 구현체에도 의존 (MemberRepository,MemoryMemberRepository 둘다 의존)
+    //DIP 위반 !
     private final MemberRepository memberRepository = new MemoryMemberRepository();
     @Override
     public void join(Member member) {
